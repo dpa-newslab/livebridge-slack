6ab73f2651bc31a27e2686b496139262027821e5
Martin Borho <martin@borho.net>
2016-11-09 15:17:00 +0100

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 from setuptools import setup, find_packages
 
-version = '0.1.0'
+version = '0.2.0'
 
 setup(name='livebridge-slack',
       version=version,
@@ -46,7 +46,7 @@ See https://github.com/dpa-newslab/livebridge-slack for more infos.
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        "livebridge>=0.10.0"
+        "livebridge>=0.11.0"
       ]
       )
 
